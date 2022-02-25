ff1202e3d0009c50998569a850623e8179ba6c84
jerryhopper <hopper.jerry@gmail.com>
2022-02-25 11:10:42 +0100

    .

--- setup.sh
+++ setup.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
+
+
+
 # this assumes sudo is installed.
 # this assumes docker-compose is installed.
 # apt install docker-compose
 
+# Check if user is root.
 if (( $EUID != 0 )); then
     echo " You need to be root!"
     exit
 fi
 
+# Make sure dockserv is executable
 chmod +x $PWD/bin/dockserv
+
+
+# Create softlinks so binary is accessible
 sudo ln -s $PWD/bin/dockserv /usr/bin/dockserv
 chmod +x /usr/bin/dockserv
 
-
+# Check if docker is installed.
 if ! foobar_loc="$(type -p "docker")" || [[ -z $foobar_loc ]]; then
   # install foobar here
   echo install docker
   sudo apt-get install docker-compose -y
 fi
 
-
+# Check if group 'docker' exists.
 if [ $(getent group docker) ]; then
   echo "group exists."
 else
@@ -28,25 +36,16 @@ else
 fi
 
 
-
+# Create user dockserve
 if id "dockserve" &>/dev/null; then
     echo 'user dockserve found.'
-    
+
 else
     echo 'Creating user dockserve.'
     sudo useradd -m dockserve
     sudo passwd dockserve
     sudo usermod -aG docker dockserve
-    
+
 fi
-
-
-#  export PATH="/home/jerryhopper/dockserv/bin:$PATH"
-#  /home/dockserve/.bashrc
-#  export PATH="$HOME/dockserv/bin:$PATH"
-
-#sudo echo "export PATH=\"$PWD/bin:\$PATH\" >> /home/dockserve/.bashrc"
-
-
-# this assumes
-
+echo "-----------------------"
+echo "usage:   dockserve --help"
